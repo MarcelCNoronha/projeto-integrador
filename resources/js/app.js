e309b98f712a93e05e2dca0a45fad7b128e01158
MarcelCNoronha <cardoso@gmail.com>
2022-11-16 21:38:08 -0300

    Arquivos Vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import LoginComponent from './components/LoginComponent.vue';
+app.component('login-component', LoginComponent);
+
+app.mount('#app');
