741fccdc62cd39a910954c7788b99e7b32af7576
MarcelCNoronha <cardoso@gmail.com>
2022-11-23 21:43:01 -0300

    Página de login, vue

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,41 +1,72 @@
 <template>
     <div class="container text-center">
-        <div class="row mt-5">
+        <div class="row">
         <div>
             <img src="/image/logo.jpg" alt="logo">
         </div>
-        <div class="mt-1" >
+        <div class="mt-0" >
             <h2>Confira as oportunidades</h2>
         </div>
-        <div class="mt-2">
+        <div class="mt-0">
             <p>Informe suas credênciais para acessar a plataforma</p>
         </div>
-        <div class="row mt-3 offset-md-4">
-        <form class="col-md-6 text-start">
+        <div class="row mt-2 offset-md-4">
+        <form class="col-md-4 text-start">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-            placeholder="Digite seu email" v-model="form.email">
+            placeholder="Digite seu email" v-model="form.email" :disabled="filledEmail">
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Digite sua senha</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite sua senha">
+            <div v-if="filledEmail && isUser" class="mb-3">
+                <label for="exampleInputPassword" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="password"
+            placeholder="Digite a sua senha" v-model="form.password">
             </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn:hover btn-text col-12" @click="getIsUser()">Entrar</button>
 
-<!--
+            <div v-if="filledEmail && !isUser">
+                <div class="mb-3">
+                    <label for="exampleInputName" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="Name"
+                    placeholder="Digite seu nome" v-model="form.name">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputPhone" class="form-label">Telefone</label>
+                    <input type="text" class="form-control" id="phone"
+                    placeholder="Digite seu telefone" v-model="form.phone">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputAge" class="form-label">Idade</label>
+                    <input type="number" class="form-control" id="age"
+                    placeholder="Digite a sua idade" v-model="form.age">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Minha senha</label>
+                    <input type="password" class="form-control" id="password"
+                    placeholder="Digite a sua senha" v-model="form.password">
+                </div>
+
+                <div class="mb-3">
+                    <label for="exampleInputPassword2" class="form-label">Repita sua senha</label>
+                    <input type="password" class="form-control" id="password"
+                    placeholder="Repita a sua senha" v-model="form.repeatPassword">
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <button type="button" class="btn btn:hover btn-text col-12" @click="getIsUser()"> Entrar</button>
+                <!-- {{isUser}} -->
+                <!--
                 <div v-for="(service,index) in services" :key="index">
                     {{service.name}}
                 </div> -->
-
             </div>
             </form>
-            </div>
-            {{isUser}}
-            </div>
-            {{isUser}}
         </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -45,9 +76,16 @@
             return {
                 form:
                 {
-                    email: ''
+                    email: '',
+                    password:'',
+                    name:'',
+                    phone:'',
+                    age:'',
+                    active: 1,
+                    repeatPassword: ''
                 },
-                isUser: ''
+                isUser: '',
+                filledEmail: false
                 // services:[]
             }
         },
@@ -58,9 +96,10 @@
             getIsUser(){
                 axios({
                     method: 'get',
-                    url: '/user/is-user/' + this.form.email
+                    url: '/api/user/is-user/' + this.form.email
                 }).then((response)=> {
-                   this.isUser =response.data.sucess;
+                   this.isUser       = response.data.sucess;
+                   this.filledEmail = true;
                 })
 
             }
